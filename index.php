cb61978dc4b42f7f47617be14cef9b9fd0e605fb
LauraKo <kovarskyte@yahoo.com>
2021-02-08 18:30:41 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<h1>Labas, Zuiki, ka tu?</h1>
+
+<?php
+
+echo 100 + 333;
+$kintamasis = 12;
+echo $kintamasis;
+echo $kintamasis++;
+echo ++$kintamasis;
+$pirmas = "bla bla";
+$antras = "ku ku";
+$trecias = $pirmas . $antras;
+echo $trecias;
+echo $pirmas;
+echo '<br>';
+echo $antras;
+
+?>
+
+<h1>Normaliai</h1>
